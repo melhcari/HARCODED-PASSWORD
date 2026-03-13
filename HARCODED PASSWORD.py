409f61d100d7022db12038401c796909eddf389a
lozsalazar33-hash <Lozsalazar33@gmail.com>
2026-03-13 15:32:05 -0600

    add harcoded password rule

--- HARCODED PASSWORD.py
+++ HARCODED PASSWORD.py
@@ -1,8 +1,13 @@
-DB_PASSWORD = "SuperSecret123"
+rules:
+- id: hardcoded-password
+  patterns:
+    - pattern: $VAR = "..."
+    - metavariable-regex:
+        metavariable: $VAR
+        regex: (?i)(password|passwd|pwd|secret|token|api_key)
+  message: Hardcoded credential detected
+  languages: [python]
+  severity: WARNING
 
-def connect():
-    print("Connecting to database...")
-    print("Password:", DB_PASSWORD)
 
-connect()
 
