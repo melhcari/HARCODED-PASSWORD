4f98542ac5839610d7158cf1a25dec36b72e78ba
lozsalazar33-hash <Lozsalazar33@gmail.com>
2026-03-13 14:46:43 -0600

    Subir

---
+++ HARCODED PASSWORD.py
@@ -0,0 +1,8 @@
+password_correcta = "admin123"
+
+password = input("Ingresa la contraseña: ")
+
+if password == password_correcta:
+    print("Acceso concedido")
+else:
+    print("Acceso denegado")
