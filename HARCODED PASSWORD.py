567a63090f93a011ed83d58aefa6cff359a49d94
lozsalazar33-hash <Lozsalazar33@gmail.com>
2026-03-13 15:57:56 -0600

    fix semgrep rule

--- HARCODED PASSWORD.py
+++ HARCODED PASSWORD.py
@@ -1,8 +1,13 @@
-password = "admin123"
-
-def login():
-    user = input("Enter password: ")
-    if user == password:
-        print("Access granted")
-
-login()
+rules:
+  - id: detect-hardcoded-password
+    languages: [python, javascript, go, java]
+    message: "Se detectó una posible contraseña grabada en el código (hardcoded). Esto es una mala práctica de seguridad."
+    severity: WARNING
+    patterns:
+      - pattern-either:
+          - pattern: $VAR = "..."
+          - pattern: $VAR = '...'
+      - metavariable-regex:
+          metavariable: $VAR
+          regex: (?i)^(password|passwd|secret|api_key|token|pwd)$
+      - pattern-not: $VAR = ""
